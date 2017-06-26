bff6f40261f747283caa460921debbcedf4affb3
jksdd88 <wang_yu@dodoca.com>
2017-06-26 18:53:02 +0800

    no message

--- swoole/use_cli_demo.php
+++ swoole/use_cli_demo.php
@@ -1,15 +1,17 @@
 <?php
-defined('FREEDOM_PATH') or define('FREEDOM_PATH', __DIR__.'/../freedom/library/');
-defined('RUNDATA_PATH') or define('RUNDATA_PATH', FREEDOM_PATH.'../../rundata/');
-define('SYS_MODULE', 'swoole');
-require FREEDOM_PATH . 'PubFun.php';
-
 /**
  * User: jksdd88
  * Date: 2017/6/26
  * Time: 17:43
  * 注：该文件函数只能用于cli命令行环境
  */
+
+defined('FREEDOM_PATH') or define('FREEDOM_PATH', __DIR__.'/../freedom/library/');
+defined('RUNDATA_PATH') or define('RUNDATA_PATH', FREEDOM_PATH.'../../rundata/');
+define('SYS_MODULE', 'swoole');
+require FREEDOM_PATH . 'PubFun.php';
+
+save_log('start > '.time(),'swoole');
 
 //异步循环间隔执行 (异步高精度定时器，粒度为毫秒级) 每隔2000ms触发一次
 $id = swoole_timer_tick(2000, function ($timer_id) {
@@ -18,9 +20,8 @@ $id = swoole_timer_tick(2000, function ($timer_id) {
 
 //异步延迟执行 (异步高精度定时器，粒度为毫秒级) 3000ms后执行此函数
 swoole_timer_after(3000, function () {
-
     save_log('swoole_timer_tick >'.time(),'swoole');
 });
-echo $id;
+echo 'start';
 //可以使用 swoole_timer_clear 清除swoole_timer_tick or swoole_timer_after定时器，参数为定时器ID
-//swoole_timer_clear($id);
+swoole_timer_clear($id);
