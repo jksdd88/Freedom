69b1a875588c5fc766e961175ff673bdbb85437a
jksdd88 <wang_yu@dodoca.com>
2017-06-26 17:50:27 +0800

    no message

--- app1/controllers/SwooleController.php
+++ app1/controllers/SwooleController.php
@@ -3,7 +3,8 @@
 class  SwooleController extends \library\Controller
 {
 	/**
-	 * 异步循环间隔执行 (异步高精度定时器，粒度为毫秒级)
+	 * 异步循环间隔执行 (异步高精度定时器，粒度为毫秒级) z
+	 * 注：只能用于cli命令行环境
 	 */
  	function timertickAction(){
 
@@ -11,13 +12,14 @@ class  SwooleController extends \library\Controller
 		$id = swoole_timer_tick(2000, function ($timer_id) {
 			save_log('swoole_timer_tick > '.json_encode($timer_id),'swoole');
 		});
-		s($id);
+
 		//可以使用 swoole_timer_clear 清除此定时器，参数为定时器ID
-//		swoole_timer_clear($id);
+		swoole_timer_clear($id);
  	}
 
 	/**
 	 * 异步延迟执行 (异步高精度定时器，粒度为毫秒级)
+	 * 注：只能用于cli命令行环境
 	 */
 	function timerafterAction(){
 
