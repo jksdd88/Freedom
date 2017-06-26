32dc052820731eb411d18b3145da646126cbe7df
jksdd88 <wang_yu@dodoca.com>
2017-06-26 18:49:17 +0800

    no message

--- swoole/use_cli_demo.php
+++ swoole/use_cli_demo.php
@@ -13,14 +13,14 @@ require FREEDOM_PATH . 'PubFun.php';
 
 //异步循环间隔执行 (异步高精度定时器，粒度为毫秒级) 每隔2000ms触发一次
 $id = swoole_timer_tick(2000, function ($timer_id) {
-    save_log('swoole_timer_tick > '.json_encode($timer_id),'swoole');
+    save_log('swoole_timer_tick > '.json_encode($timer_id).' time > '.time(),'swoole');
 });
 
 //异步延迟执行 (异步高精度定时器，粒度为毫秒级) 3000ms后执行此函数
-$time_now = time();
 swoole_timer_after(3000, function () {
-    save_log('swoole_timer_tick > '. $time_now . ' '.time(),'swoole');
-});
 
+    save_log('swoole_timer_tick >'.time(),'swoole');
+});
+echo $id;
 //可以使用 swoole_timer_clear 清除swoole_timer_tick or swoole_timer_after定时器，参数为定时器ID
 //swoole_timer_clear($id);
